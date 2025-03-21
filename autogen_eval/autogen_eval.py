f07d8879ec120e87649ba23c22a8b27e0659fd0d
吴子豪 <vortezwohl@proton.me>
2025-03-22 01:07:53 +0800

    update 新增autogen实验

--- autogen_eval/autogen_eval.py
+++ autogen_eval/autogen_eval.py
@@ -1,38 +1,30 @@
 import asyncio
 from autogen_agentchat.agents import AssistantAgent
-from autogen_agentchat.messages import TextMessage
 from autogen_agentchat.ui import Console
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 from dotenv import load_dotenv
 
+from tool import search, move, use, check
+
 load_dotenv()
 
 
-def calculator(a: float, b: float, operator: str) -> str:
-    try:
-        if operator == '+':
-            return str(a + b)
-        elif operator == '-':
-            return str(a - b)
-        elif operator == '*':
-            return str(a * b)
-        elif operator == '/':
-            if b == 0:
-                return 'Error: Division by zero'
-            return str(a / b)
-        else:
-            return 'Error: Invalid operator. Please use +, -, *, or /'
-    except Exception as e:
-        return f'Error: {str(e)}'
+model_client = OpenAIChatCompletionClient(
+    model="gpt-4o-mini",
+)
 
 
-async def main():
-    model_client = OpenAIChatCompletionClient(model="gpt-4o-mini")
-    agent = AssistantAgent("assistant", model_client=model_client, tools=[calculator])
-    await Console(
-        agent.on_messages_stream(
-            [TextMessage(content="What is the result of 123.45 * 67.89?", source="user")]
-        )
-    )
+agent = AssistantAgent(
+    name="agent",
+    model_client=model_client,
+    tools=[search, move, use, check],
+    system_message="You are a helpful assistant.",
+    reflect_on_tool_use=True,
+    model_client_stream=True,  # Enable streaming tokens from the model client.
+)
+
+
+async def main() -> None:
+    await Console(agent.run_stream(task="去厨房看看"))
 
 asyncio.run(main())
---
+++ autogen_eval/model.py
@@ -0,0 +1,5 @@
+from ceo import get_openai_model
+from dotenv import load_dotenv
+
+load_dotenv()
+model = get_openai_model()
